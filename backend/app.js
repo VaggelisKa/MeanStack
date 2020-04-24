0346f503c266005bdac193847a4d009ce22945da
VaggelisKa <e.karavasileiadis@gmail.com>
2020-04-24 14:28:17 +0300

    Adding POST backend point.

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,10 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -12,7 +16,15 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use('/api/posts', (req, res, next) => {
+app.post('/api/posts', (req, res, next) => {
+    const post = req.body;
+    console.log(post);
+    res.status(201).json({
+        message: 'Post added'
+    });
+});
+
+app.get('/api/posts', (req, res, next) => {
     const posts = [
        {
            id: 'dadasasa', 

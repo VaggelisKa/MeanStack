e5fa40ec69788634e80e31b06ba2eb271b0555ab
VaggelisKa <e.karavasileiadis@gmail.com>
2020-05-01 14:29:35 +0300

    Backend pagination

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -44,14 +44,22 @@ router.post('', multer({storage: storage}).single('image'),(req, res, next) => {
 });
 
 router.get('', (req, res, next) => {
-    Post.find()
-        .then(documents => {
-            console.log(documents);
-            res.status(200).json({
-                message: 'posts fetched successfully',
-                posts: documents
-            });
+    const pageSize = +req.query.pagesize;
+    const currentPage = +req.query.page;
+    const postQuery = Post.find();
+    if (pageSize && currentPage) {
+        postQuery
+            .skip(pageSize * (currentPage - 1))
+            .limit(pageSize);
+    }
+
+    postQuery.then(documents => {
+        console.log(documents);
+        res.status(200).json({
+            message: 'posts fetched successfully',
+            posts: documents
         });
+    });
 });
 
 router.get('/:id', (req, res, next) => {

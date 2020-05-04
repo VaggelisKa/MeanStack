cff924d150e788d5230880148ca0e8ceec7abcab
VaggelisKa <e.karavasileiadis@gmail.com>
2020-05-04 21:49:34 +0300

    Saving the token, expirationDate and username on the local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Post } from './posts/models/post.model';
+import { UsersService } from './auth/services/users.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  posts: Post[] = [];
+export class AppComponent implements OnInit {
+  constructor(private usersService: UsersService) {}
 
-  onPostAdded(post) {
-    this.posts.push(post);
+  ngOnInit() {
+    this.usersService.autoAuthUser();
   }
 }
--- src/app/auth/services/users.service.ts
+++ src/app/auth/services/users.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthData } from '../models/auth-data.model';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class UsersService {
@@ -18,7 +17,7 @@ export class UsersService {
         return this.token;
     }
 
-    getUsername() {
+    getUsername(): Observable<string> {
         return this.username.asObservable();
     }
 
@@ -59,20 +58,74 @@ export class UsersService {
                 this.isAuthLoading.next(false);
                 this.token = response.token;
 
-                if (this.token) {
-                    this.tokenTimer = setTimeout(() => {
-                        this.logout();
-                    }, response.expiresIn * 1000);
+                this.setTokenTimer(response.expiresIn);
 
+                if (this.token) {
                     this.username.next(response.username);
                     this.authStatusListener.next(true);
+
+                    const currentDate = new Date();
+                    this.saveAuthData(
+                        response.username,
+                        this.token,
+                        new Date(currentDate.getTime() + response.expiresIn * 1000)
+                        );
                 }
             });
+    }
+
+    autoAuthUser() {
+        const currentDate = new Date();
+        if (!this.getAuthData()) {
+            return;
+        }
+        const expiresIn = this.getAuthData().expirationDate.getTime() - currentDate.getTime();
+        if (expiresIn > 0) {
+            this.token = this.getAuthData().token;
+            this.setTokenTimer(expiresIn / 1000);
+            this.authStatusListener.next(true);
+            this.username.next(this.getAuthData().username);
+        }
     }
 
     logout() {
         this.token = null;
         clearTimeout(this.tokenTimer);
         this.authStatusListener.next(false);
+        this.clearAuthData();
+    }
+
+    private saveAuthData(username: string, token: string, expirationDate: Date) {
+        localStorage.setItem('username', username);
+        localStorage.setItem('token', token);
+        localStorage.setItem('expiration', expirationDate.toISOString());
+    }
+
+    private setTokenTimer(duration: number) {
+        console.log('setting timer: ' + duration);
+        this.tokenTimer = setTimeout(() => {
+            this.logout();
+        }, duration * 1000);
+    }
+
+    private clearAuthData() {
+        localStorage.removeItem('username');
+        localStorage.removeItem('token');
+        localStorage.removeItem('expiration');
+    }
+
+    private getAuthData(): {username: string, token: string, expirationDate: Date} {
+        const username = localStorage.getItem('username');
+        const token = localStorage.getItem('token');
+        const expirationDate = localStorage.getItem('expiration');
+        if (!token || !expirationDate) {
+            return;
+        }
+
+        return {
+            username,
+            token,
+            expirationDate: new Date(expirationDate)
+        };
     }
 }
